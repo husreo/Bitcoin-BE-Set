eed5f3677caf9d43d84f8057b4a154beaebffa89
husreo <springers230@gmail.com>
2024-09-13 14:58:29 +0900

    feat:utxo tst

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-console.log("Hello world");
+import { getUTXO } from "./manageUTXO/utxo";
+
+(async () => {
+    const result = await getUTXO("tb1pknuxraa3mwhq2t3arz52n3lnnrs39kzpt6u2v8errf5qqwvfg25qvkxeyu", "testnet");
+    console.log(result);
+})()
---
+++ manageUTXO/utxo.ts
@@ -0,0 +1,115 @@
+import axios from "axios";
+import * as Bitcoin from "bitcoinjs-lib";
+import networkConfig from "../config/network.config";
+
+interface IUtxo {
+    txid: string;
+    vout: number;
+    value: number;
+}
+const networkType: string = networkConfig.networkType;
+
+export const getUTXO = async (address:string, networkType: string): Promise<IUtxo[]> => {
+    const url = `https://mempool.space/${networkType}/api/address/${address}/utxo`;
+    const res = await axios.get(url);
+    const utxos: IUtxo[] = [];
+    
+    res.data.forEach((utxoData: any) => {
+        utxos.push({
+            txid: utxoData.txid,
+            vout: utxoData.vout,
+            value: utxoData.value,
+        })
+    });
+
+    return utxos;
+}
+
+export const selectUTXO = async (utxos:IUtxo[], targetAmount: number, initialFee: number): Promise<IUtxo[]> => {
+    const selectUTXO: IUtxo[] = [];
+    utxos.sort((a, b) => a.value - b.value);
+    let sum = targetAmount + initialFee;
+    const utxo = utxos.find((utxo) => utxo.value >= sum);
+
+    if (utxo != undefined) {
+        selectUTXO[0] = utxo;
+        return selectUTXO;
+    }
+
+    for (let i = utxos.length; i > 0; i--) {
+        sum = sum - utxos[i - 1].value;
+        selectUTXO[utxos.length - i] = utxos[i];
+        if (sum <= 0) break;
+    }
+
+    if (sum >= 0) throw new Error("No btcs");
+    return selectUTXO;
+}
+
+export const createPsbt = async (selectedUtxo: IUtxo[], amount: number, wallet: any, receivedAddress: any, fee: number): Promise<Bitcoin.Psbt> => {
+    let value = 0;
+
+    const psbt = new Bitcoin.Psbt({
+        network: networkType == "testnet" ? Bitcoin.networks.testnet : Bitcoin.networks.bitcoin
+    })
+
+    for (let i = 0; i < selectedUtxo.length; i++) {
+        psbt.addInput({
+            hash: selectedUtxo[i].txid,
+            index: selectedUtxo[i].vout,
+            witnessUtxo: {
+                value: selectedUtxo[i].value,
+                script: wallet.output,
+            },
+            tapInternalKey: Buffer.from(wallet.publicKey, "hex").subarray(1, 33),
+        })
+        value += selectedUtxo[i].value;
+    }
+
+    psbt.addOutput({
+        address: wallet.address,
+        value: value - amount - fee,
+    })
+
+    psbt.addOutput({
+        address: receivedAddress,
+        value: amount,
+    })
+
+    return psbt;
+}
+
+export const createSplitPsbt = async (selectedUtxo: IUtxo[], amount: number, wallet: any, receivedAddress: any, splitNum: number, fee: number): Promise<Bitcoin.Psbt> => {
+    let value = 0;
+
+    const psbt = new Bitcoin.Psbt({
+        network: networkType == "testnet" ? Bitcoin.networks.testnet : Bitcoin.networks.bitcoin
+    })
+
+    for (let i = 0; i < selectedUtxo.length; i++) {
+        psbt.addInput({
+            hash: selectedUtxo[i].txid,
+            index: selectedUtxo[i].vout,
+            witnessUtxo: {
+                value: selectedUtxo[i].value,
+                script: wallet.output,
+            },
+            tapInternalKey: Buffer.from(wallet.publicKey, "hex").subarray(1, 33),
+        })
+        value += selectedUtxo[i].value;
+    }
+
+    for (let i = 0; i < splitNum; i++) {
+        psbt.addOutput({
+            address: receivedAddress,
+            value: amount / splitNum,
+        })
+    }
+
+    psbt.addOutput({
+        address: receivedAddress,
+        value: value - amount - fee,
+    })
+
+    return psbt;
+}
