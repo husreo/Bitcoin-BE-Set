fa7def46111e793276bdb77de3471b3c26726b8d
husreo <springers230@gmail.com>
2024-09-25 18:04:57 +0900

    feat:psbt

--- bundler/ordinals/ordinals_transfer.ts
+++ bundler/ordinals/ordinals_transfer.ts
@@ -2,83 +2,89 @@ import { SeedWallet } from "../../wallets/SeedWallet";
 import networkConfig from "../../config/network.config";
 import * as Bitcoin from 'bitcoinjs-lib';
 import axios from "axios";
+import { getUTXO, selectUTXO } from "../../manageUTXO/utxo";
 
 const networkType: string = networkConfig.networkType;
+const INITIAL_FEE = 400;
 
-// export async function sendOrdinalsController(
-//     destination: string,
-//     inscriptionId: string,
-//     paymentAddress: string
-// ) {
+export async function sendOrdinalsController(
+    destination: string,
+    inscriptionId: string,
+    paymentAddress: string
+) {
+    let initialFee = 0;
+    let redeemFee = INITIAL_FEE;
+    const psbt = new Bitcoin.Psbt({
+        network: networkType == "testnet" ? Bitcoin.networks.testnet : Bitcoin.networks.bitcoin
+    });
+    const seed: string = process.env.MNEMONIC as string;
+    const wallet = new SeedWallet({ networkType: networkType, seed: seed });
 
-//     const psbt = new Bitcoin.Psbt({
-//         network: networkType == "testnet" ? Bitcoin.networks.testnet : Bitcoin.networks.bitcoin
-//     });
-//     const seed: string = process.env.MNEMONIC as string;
-//     const wallet = new SeedWallet({ networkType: networkType, seed: seed });
+    const inscriptionData = await getInscriptionData(
+        wallet.address,
+        inscriptionId
+        // "e27c4838659659036fbdbbe869a49953d7fc65af607b160cff98736cea325b1ei0"
+    );
 
-//     const inscriptionData = await getInscriptionData(
-//         wallet.address,
-//         inscriptionId
-//         // "e27c4838659659036fbdbbe869a49953d7fc65af607b160cff98736cea325b1ei0"
-//     );
+    psbt.addInput({
+        hash: inscriptionData.txid,
+        index: inscriptionData.vout,
+        tapInternalKey: Buffer.from(wallet.publicKey, "hex").subarray(1, 33),
+        witnessUtxo: {
+            script: wallet.output,
+            value: inscriptionData.satoshi,
+        },
+    });
 
-//     psbt.addInput({
-//         hash: inscriptionData.txid,
-//         index: inscriptionData.vout,
-//         witnessScript: Buffer.from(multisigVault.witnessScript, "hex"),
-//         witnessUtxo: {
-//             script: Buffer.from(multisigVault.p2msOutput, "hex"),
-//             value: inscriptionData.satoshi,
-//         },
-//     });
+    psbt.addOutput({
+        address: destination,
+        value: inscriptionData.satoshi,
+    });
 
-//     psbt.addOutput({
-//         address: destination,
-//         value: inscriptionData.satoshi,
-//     });
+    const btcUtxos = await getUTXO(wallet.address, "testnet");
+    do {
+        initialFee = redeemFee;
+        const selectedUTXO = await selectUTXO(btcUtxos, 0, initialFee);
+    } while (redeemFee != initialFee);
+    // const feeRate = (await getFeeRate()) + 400;
+    // console.log("feeRate ==> ", feeRate);
+    // let FinalTotalBtcAmount = 0;
+    // let finalFee = 0;
+    // for (const btcutxo of btcUtxos) {
+    //     finalFee = await calculateTxFee(psbt, feeRate);
+    //     if (FinalTotalBtcAmount < finalFee && btcutxo.value > 10000) {
+    //         FinalTotalBtcAmount += btcutxo.value;
+    //         psbt.addInput({
+    //             hash: btcutxo.txid,
+    //             index: btcutxo.vout,
+    //             witnessScript: Buffer.from(witnessScript, "hex"),
+    //             witnessUtxo: {
+    //                 script: Buffer.from(p2msOutput, "hex"),
+    //                 value: btcutxo.value,
+    //             },
+    //         });
+    //     }
+    // }
 
-//     const btcUtxos = await getBtcUtxoByAddress(address);
+    // console.log("Pay finalFee =====================>", finalFee);
 
-//     const feeRate = (await getFeeRate()) + 400;
-//     console.log("feeRate ==> ", feeRate);
-//     let FinalTotalBtcAmount = 0;
-//     let finalFee = 0;
-//     for (const btcutxo of btcUtxos) {
-//         finalFee = await calculateTxFee(psbt, feeRate);
-//         if (FinalTotalBtcAmount < finalFee && btcutxo.value > 10000) {
-//             FinalTotalBtcAmount += btcutxo.value;
-//             psbt.addInput({
-//                 hash: btcutxo.txid,
-//                 index: btcutxo.vout,
-//                 witnessScript: Buffer.from(witnessScript, "hex"),
-//                 witnessUtxo: {
-//                     script: Buffer.from(p2msOutput, "hex"),
-//                     value: btcutxo.value,
-//                 },
-//             });
-//         }
-//     }
+    // if (FinalTotalBtcAmount < finalFee)
+    //     throw `Need more ${finalFee - FinalTotalBtcAmount} BTC for transaction`;
 
-//     console.log("Pay finalFee =====================>", finalFee);
+    // console.log("FinalTotalBtcAmount ====>", FinalTotalBtcAmount);
 
-//     if (FinalTotalBtcAmount < finalFee)
-//         throw `Need more ${finalFee - FinalTotalBtcAmount} BTC for transaction`;
+    // psbt.addOutput({
+    //     address: address,
+    //     value: FinalTotalBtcAmount - finalFee,
+    // });
 
-//     console.log("FinalTotalBtcAmount ====>", FinalTotalBtcAmount);
+    // console.log("psbt.toHex() ==> ", psbt.toHex());
 
-//     psbt.addOutput({
-//         address: address,
-//         value: FinalTotalBtcAmount - finalFee,
-//     });
-
-//     console.log("psbt.toHex() ==> ", psbt.toHex());
-
-//     return psbt.toHex();
-// }
+    return psbt.toHex();
+}
 
 export const getInscriptionData = async (
-    // address: string,
+    address: string,
     inscriptionId: string
 ) => {
     const seed: string = process.env.MNEMONIC as string;
--- index.ts
+++ index.ts
@@ -6,6 +6,6 @@ Bitcoin.initEccLib(ecc);
 
 import { getInscriptionData } from './bundler/ordinals/ordinals_transfer';
 (async() => {
-    const utxo = await getInscriptionData("e875480e44ec88c1ea9cd5946e1c504619410ee4e17a99eef9a3f7344f35efcci0");
-    console.log(utxo.utxo.inscriptions);
+    // const utxo = await getInscriptionData("e875480e44ec88c1ea9cd5946e1c504619410ee4e17a99eef9a3f7344f35efcci0");
+    // console.log(utxo.utxo.inscriptions);
 })()
